89ab07c1644f3725f799b9c878973ef526f89ad5
Rapture <m13422298544@163.com>
2026-03-23 19:49:41 +0800

    判断2幂次方简单思路

---
+++ 0231.isPowerOfTwo1.c
@@ -0,0 +1,15 @@
+bool isPowerOfTwo(int n) {
+    if (n == 1) {
+        return true;
+    }
+    if (n <= 0 || n & 1) {
+        return false;
+    }
+    while (n >= 2) {
+        if (n & 1) {
+            return false;
+        }
+        n /= 2;
+    }
+    return true;
+}
